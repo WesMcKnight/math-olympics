710712f9da62214f3878a43ede94b4316e182d61
Wes McKnight <wesleymcknight17@gmail.com>
2023-05-24 11:49:17 -0500

    reactstrap works now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Row, Container, Nav, Button } from 'reactstrap';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      This is where all of the stuff will go!
-    </div>
+    <Container>
+      <Row>
+        <h1>Welcome to the Math Olympics!</h1>
+      </Row>
+      <Nav>
+        <Button type='button' color='blue'>Click ME!</Button>
+      </Nav>
+    </Container>
+
   );
 }
 
