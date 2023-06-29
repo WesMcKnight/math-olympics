b96117f6b0e1145c38665360742e7e5315bace2e
Wes McKnight <wesleymcknight17@gmail.com>
2023-06-28 20:54:23 -0500

    addition testing

--- src/Addition.js
+++ src/Addition.js
@@ -1,7 +1,16 @@
+import { Modal } from "reactstrap";
 
 const Addition = () => {
     console.log("it works for addition!");
     console.log("Hopefully this will work as well")
+
+    return(
+        <Modal
+            fade
+        >
+            <div>it works!</div>
+        </Modal>
+    )
 }
 
 export default Addition;
--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import './App.css';
 import Addition from './Addition';
 
 function App() {
-  // const[score, setScore] = useState(() =>{
-
-  // })
+  const[score, setScore] = useState()
+  
   return (
     <>
       <h1>Welcome to the Math Olympics!</h1>
       <div>
         <span>
           Here's your current score:
-          <div id='score'>score</div>
+          <div>score</div>
         </span>
         <h3>Which math challenge do you want?</h3>
-        <button type='button' onClick= {Addition}>Addition</button>
-        
+        <h4>Addition:</h4>
+        <Addition />
       </div>
     
     </>
